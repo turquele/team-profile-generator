76c370758059c1790d0a7aa032d56dde633a296f
Jose Alonso <jose.m.alonso.g@gmail.com>
2023-02-28 22:55:00 +0000

    added screenshot

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+var team = []
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
