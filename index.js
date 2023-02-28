7afde14cbb402ecc5f825cecb28db33ef599e67e
Jose Alonso <jose.m.alonso.g@gmail.com>
2023-02-28 22:40:34 +0000

    added functions to gather information

--- index.js
+++ index.js
@@ -12,4 +12,98 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+function getManagerInfo() {
+    inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: `Name of the team manager's`,
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'Employee ID',
+        name: 'id'
+    },
+    {
+        type: 'input',
+        message: 'e-mail',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'Office number',
+        name: 'office'
+    },
+  ])
+  .then((response) => {
+    team.push(new Manager(response.name, response.id, response.email, response.office));
+    console.log(`Employee ${response.id} successfully added.
+    `)
+    menuOptions();
+  });
+}
 
+function getEngineerInfo() {
+    inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: `Engineer's name`,
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'Employee ID',
+        name: 'id'
+    },
+    {
+        type: 'input',
+        message: 'e-mail',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'Github username',
+        name: 'github'
+    },
+  ])
+  .then((response) => {
+    team.push(new Engineer(response.name, response.id, response.email, response.github));
+    console.log(`Employee ${response.id} successfully added.
+    `)
+    menuOptions();
+  });
+}
+
+function getInternInfo() {
+    inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: `Intern's name`,
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'Employee ID',
+        name: 'id'
+    },
+    {
+        type: 'input',
+        message: 'e-mail',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'School',
+        name: 'school'
+    },
+  ])
+  .then((response) => {
+    team.push(new Intern(response.name, response.id, response.email, response.school));
+    console.log(`Employee ${response.id} successfully added.
+    `)
+    menuOptions();
+  });
+}
